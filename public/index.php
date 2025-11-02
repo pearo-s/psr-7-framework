7739659703ba5f0621a7b4fb831a7f1fa07838db
Sultan Bekbosun <sultan.bekbosun@sibers.com>
2025-11-03 01:08:55 +0600

    Step 1/7. Day 1.

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+use Framework\Http\Request;
+
+chdir(dirname(__DIR__));
+require 'vendor/autoload.php';
+
+$request = new Request();
+
+$name = $request->getQueryParams()['name'] ?? 'Guest';
+
+header('X-Developer: suck');
+echo 'Hello, ' . $name . PHP_EOL;
