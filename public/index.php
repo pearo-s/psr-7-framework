30a419311f0697b0d2139e70b0b4c6b3e7961fa1
Sultan Bekbosun <sultanbb28@gmail.com>
2026-01-08 18:24:13 +0600

    creating router and controller day 1

--- public/index.php
+++ public/index.php
@@ -1,18 +1,36 @@
 <?php
 
 use Laminas\Diactoros\Response\HtmlResponse;
+use Laminas\Diactoros\Response\JsonResponse;
 use Laminas\Diactoros\ServerRequestFactory;
 use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
 
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
 
+// Initialization
+
 $request = ServerRequestFactory::fromGlobals();
 
-$name = $request->getQueryParams()['name'] ?? 'Guest';
+// Action
 
-$response = (new HtmlResponse('Hello, ' . $name . '!'))
-    ->withHeader('X-Developer', 'ElisDN');
+$path = $request->getUri()->getPath();
+
+if ($path === '/') {
+    $name = $request->getQueryParams()['name'] ?? 'Guest';
+    $response = new HtmlResponse('Hello, ' . $name . '!');
+} elseif ($path === '/about') {
+    $response = new HtmlResponse('I am a simple site');
+} else {
+    $response = new JsonResponse(['error' => 'Undefined page'], 404);
+}
+
+
+// Postprocessing
+
+$response = $response->withHeader('X-Developer', 'ElisDN');
+
+// Sending
 
 $emitter = new SapiEmitter();
 $emitter->emit($response);
